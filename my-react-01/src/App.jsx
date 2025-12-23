7254a277bde9fe4e4f26e3ad2d04e46525aa33fb
Adiba Islam <iadiba7936@gmail.com>
2025-12-23 10:34:48 +0530

    learn how to get time display along day & year in app component

--- my-react-01/src/App.jsx
+++ my-react-01/src/App.jsx
@@ -1,17 +1,44 @@
 import React from 'react'
 
 const App = () => {
+           
+  const today = new Date();
+
   return (
-    <div id = 'parent'>Finally learning
+    <>
+    <div id = 'parent'>
+      <p>Finally learning React.js [Alhundulillah] </p>
+
         <h1 id = 'child1'> App</h1>
-
         <h1 id = 'child2'> App</h1>
-</div>
-  )
-}
 
-export default App
+        <h2> Show current year!!</h2>
+
+        <h3> {new Date().getFullYear()}</h3>
+        <h3> { new Date().getDay()}</h3>
+
+
+            <h4>
+              {today.toLocaleDateString('en-us',{
+                weekday: 'long',
+                day: 'numeric',
+                  month: 'long',
+                  year: 'numeric',
+                  hour: 'numeric',
+                  minute: 'numeric',
+                  second: 'numeric',
+ })}
+            </h4>
+          
+</div>
+</>
+
+  );
+};
+
+export default App // this line allow to render it in index.js
 
 
 
 // This is the react arror function
+// insted of div we use fragments <> </> for better and clear code
