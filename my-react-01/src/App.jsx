9e1a692e3c1a457da79cfadc571df78f94de7650
Adiba Islam <iadiba7936@gmail.com>
2025-12-23 09:23:28 +0530

    my-react-01 done

---
+++ my-react-01/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div id = 'parent'>Finally learning
+        <h1 id = 'child1'> App</h1>
+
+        <h1 id = 'child2'> App</h1>
+</div>
+  )
+}
+
+export default App
+
+
+
+// This is the react arror function
