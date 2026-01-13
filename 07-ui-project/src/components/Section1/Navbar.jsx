626b3190a28d6db4452f21cd5ebd970731aa2dc6
Adiba Islam <iadiba7936@gmail.com>
2026-01-13 23:04:58 +0530

    Started Ui project

---
+++ 07-ui-project/src/components/Section1/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex item-center justify-between px10 py-6'>
+        <h4 className='bg-black text-white px-18 py-8 rounded-full uppercase'>Target Audience </h4>
+        <button className='bg-gray-300 py-2 px-6 uppercase rounded-full tracking-wider text-sm'> Digital Banking Platform</button>
+    </div>
+  )
+}
+
+export default Navbar
