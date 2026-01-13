9023cb8747ab5f411634bd331e533cf04be3e1a2
Adiba Islam <iadiba7936@gmail.com>
2026-01-13 16:59:15 +0530

    Learned Css modules and css

---
+++ 05-css/src/components/button/Button.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './Button.module.css'
+
+
+
+const Button = () => {
+  return (
+    <div className={styles.btn}>
+      <h5>This is the React css learning </h5>
+      <button className='button'> Login </button>
+     
+      </div>
+  )
+}
+
+export default Button
